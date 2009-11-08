5de3ade8c2639d08b728553264d6bf0360d75181
Vincent Launchbury <vincent@doublecreations.com>
2009-11-08 05:37:00 -0500

    Added license/description

--- nsuds.c
+++ nsuds.c
@@ -1,3 +1,22 @@
+/* nsuds - The ncurses sudoku program
+ * Text-graphical sudoku with campaign or free-play
+ * Copyright (C) Vincent Launchbury 2009.
+ * -------------------------------------------
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
